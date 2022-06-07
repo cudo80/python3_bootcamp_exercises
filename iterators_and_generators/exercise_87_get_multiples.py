cf9635c3b5549422cdafa64266249174d2dd28df
Luiz Henrique Cudo <ineedsomespace@gmail.com>
2022-06-07 16:31:33 -0300

    more exercises

---
+++ iterators_and_generators/exercise_87_get_multiples.py
@@ -0,0 +1,12 @@
+'''
+Write a function called get_multiples, wich accepts a number and a count,
+and returns a generator that yelds the first count multiples of the number.
+The default number should be 1 and the default count should be 10.
+'''
+
+def get_multiples(number=1, count=10):
+    next_num = number
+    while count > 0:
+        yield next_num
+        count -= 1
+        next_num += number
